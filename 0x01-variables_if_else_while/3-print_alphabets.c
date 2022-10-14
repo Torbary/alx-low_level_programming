4caf3b6ac562b75829a4c1396fe770059e9ff300
Torbary <torbarey020@gmail.com>
2022-10-14 10:01:43 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+  * main -Entry point
+  *
+  * Return: ALways (0) Success
+  *
+  */
+
+int main(void)
+{
+	char alphabet = 'a';
+	char ch;
+
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	for (ch = 'A' ; ch = 'Z' ; ch++)
+	{
+		putchar (ch);
+	}
+	return (0);
+}
