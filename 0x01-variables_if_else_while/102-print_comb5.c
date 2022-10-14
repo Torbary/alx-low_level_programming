c2b7430e4201719246e09e4894a7009c58c95d8d
Torbary <torbarey020@gmail.com>
2022-10-14 14:46:39 -0700

    program that prints all possible combinations of two two-digit numbers.

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,6 +12,8 @@ int main(void)
 
 	for (i = 0; i < 99; i++)
 	{
+	for (j = i + 1; j <= 99; j++)
+	{
 		putchar ('0' + i / 10);
 		putchar ('0' + i % 10);
 
@@ -24,6 +26,7 @@ int main(void)
 			break;
 		putchar (',');
 		putchar (' ');
+	}
 	}
 	putchar ('\n');
 	return (0);
