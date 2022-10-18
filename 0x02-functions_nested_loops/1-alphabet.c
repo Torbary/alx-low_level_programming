167d57227bc7fa86a9823e96210bf2c7dd63ff53
Torbary <torbarey020@gmail.com>
2022-10-18 06:56:22 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * print_alphabet -> print the lowercase alphabet
+  *
+  * Return: Always 0
+  */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar (i);
+	}
+	_putchar (i);
+
+	return (0);
+}
