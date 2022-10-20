a0339b86ebd2fc82de93e453331bae920e83cd97
Torbary <torbarey020@gmail.com>
2022-10-20 07:17:55 -0700

    printmostno

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
-  *print_most_numbers - print numbers
-  *
-  * Return: void
-  */
+*print_most_numbers - prints
+* owned by Bwave ICT
+*Return: void
+*/
 
 void print_most_numbers(void)
 {
