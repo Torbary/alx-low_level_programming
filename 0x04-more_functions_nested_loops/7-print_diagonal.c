3122f665249da33513a919d5b9295d5175a87257
Torbary <torbarey020@gmail.com>
2022-10-20 08:38:17 -0700

    printdiagonalline

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
-  * print_diagonal -printing diagonal lines
-  * @n: argument passed
-  *
-  * Return: void
-  */
+ * print_diagonal - printing lines diagonal
+ * @n: input variable
+ *
+ * Return: void
+ */
+
 
 void print_diagonal(int n)
 {
@@ -15,11 +16,12 @@ void print_diagonal(int n)
 		_putchar('\n');
 	for (i = 0; i < n; i++)
 	{
-		for (j = 0; j < 1; j++)
+		for (j = 0; j < i; j++)
 		{
 			_putchar(' ');
 		}
 		_putchar('\\');
 		_putchar('\n');
 	}
+
 }
