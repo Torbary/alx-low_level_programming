47025eb9ba4bc039a740ec733f47127d8c682c3a
Torbary <torbarey020@gmail.com>
2023-03-23 00:43:52 -0700

    island perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+
+def island_perimeter(grid):
+    """
+    Computes the perimeter of the island described in the given grid
+
+    Args:
+        grid (List[List[int]]): The rectangular grid of 0's and
+        1's representing the island.
+
+    Returns:
+        int: The perimeter of the island
+
+    Raises:
+        ValueError: if the grid is empty
+    """
+
+    if not grid:
+        raise ValueError("Grid must not be empty.")
+
+    perimeter = 0
+    height = len(grid)
+    width = len(grid[0])
+
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                """if this is a land cell"""
+                perimeter += 4
+
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+    return perimeter
