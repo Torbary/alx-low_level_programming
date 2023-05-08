059e3b6a8336255d27e6fcf32c1ec4346bfc19fe
Torbary <torbarey020@gmail.com>
2023-05-08 22:48:01 +0100

    header file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef SEARCH_ALGORITHM
+#define SEARCH_ALGORITHM
+
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
